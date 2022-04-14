f91a9f8b279e360b3cde024fe54ca43cac3d938a
Shampad Sharkar <sompodsign@gmail.com>
2022-04-15 01:51:58 +0600

    course module update view, content create view

--- courses/views.py
+++ courses/views.py
@@ -2,8 +2,14 @@ from django.views.generic.list import ListView
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.shortcuts import redirect, get_object_or_404
+from django.views.generic.base import TemplateResponseMixin, View
+from django.forms.models import modelform_factory
+from django.apps import apps
+from .forms import ModuleFormset
 
-from .models import Course
+
+from .models import Course, Module, Content
 
 class OwnerMixin(object):
     def get_queryset(self):
@@ -45,3 +51,75 @@ class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
 class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = 'course/delete.html'
     permission_required = 'courses.delete_course'
+
+
+class CourseModuleUpdateView(TemplateResponseMixin, View):
+    template_name = 'course/module/formset.html'
+    course = None
+
+    def get_formset(self, data=None):
+        return ModuleFormset(instance=self.course,
+                             data=data)
+
+    def dispatch(self, request, *args, **kwargs):
+        self.course = get_object_or_404(Course,
+                                        id=kwargs.get('pk'),
+                                        owner=request.user)
+        return super().dispatch(request, *args, **kwargs)  # pk is the course id
+
+    def get(self, request, *args, **kwargs):
+        formset = self.get_formset()
+        return self.render_to_response({'course': self.course,
+                                        'formset': formset})
+
+    def post(self, request, *args, **kwargs):
+        formset = self.get_formset(data=request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect('manage_course_list')
+        return self.render_to_response({'course': self.course,
+                                        'formset': formset})
+
+
+class ContentCreateUpdateView(TemplateResponseMixin, View):
+    module = None
+    model = None
+    obj = None
+    template_name = 'course/manage/content/form.html'
+
+    def get_model(self, model_name):
+        if model_name in ['text', 'video', 'image', 'file']:
+            return apps.get_model(app_label='courses', model_name=model_name)
+        return None
+
+    def get_form(self, model, *args, **kwargs):
+        Form = modelform_factory(model, exclude=['owner', 'order', 'created', 'updated'])
+        return Form(*args, **kwargs)
+
+    def dispatch(self, request, *args, **kwargs):
+        self.module = get_object_or_404(Module, id=kwargs.get('module_id'), course__owner=request.user)
+        self.model = self.get_model(kwargs.get('model_name'))
+        if id:
+            self.obj = get_object_or_404(self.model,
+                                         id=id,
+                                         owner=request.user)
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, module_id, model_name, id=None):
+        form = self.get_form(self.model, instance=self.obj)
+        return self.render_to_response({'form': form, 'object': self.obj})
+
+    def post(self, request, module_id, model_name, id=None):
+        form = self.get_form(self.model,
+                             instance=self.obj,
+                             data=request.POST,
+                             files=request.FILES)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.owner = request.user
+            obj.save()
+            if not id:
+                # new content
+                Content.objects.create(module=self.module, item=obj)
+            return redirect('module_content_list', self.module.id)
+        return self.render_to_response({'form': form, 'object': self.obj})
